d422e2c68e01e4679e0cda455a26f6834521de0c
David Durost <david.durost@gmail.com>
2025-11-19 19:44:31 -0500

    Test simple User helper instead of signal-bound profile creation

--- tests.py
+++ tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from user.models import User
+
+
+class UserModelTests(TestCase):
+    def test_full_name_is_combined(self):
+        user = User.objects.create_user(
+            username="full.name",
+            password="testpass123",
+            user_type=User.UserType.ADMIN,
+            first_name="Full",
+            last_name="Name",
+        )
+        self.assertEqual(user.get_full_name(), "Full Name")
