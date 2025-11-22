89e2d341053c5e7a721a868a4bbe293f9ae1f56b
David Durost <david.durost@gmail.com>
2025-11-22 07:31:11 -0500

    Make activation emails use site settings

--- signals.py
+++ signals.py
@@ -1,28 +1,46 @@
 # user/signals.py
 
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.urls import reverse
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
+from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from user.models import User
 
+
 @receiver(post_save, sender=User)
 def send_activation_email(sender, instance, created, **kwargs):
-    if created and not instance.is_active:
-        subject = "Activate Your Account"
-        uid = urlsafe_base64_encode(force_bytes(instance.pk))
-        token = default_token_generator.make_token(instance)
-        activation_link = reverse("activate", kwargs={"uidb64": uid, "token": token})
-        activation_url = f"http://yourdomain.com{activation_link}"
+    if not created or instance.is_active or not instance.email:
+        return
 
-        message = render_to_string("email/activation_email.html", {
+    base_url = getattr(settings, "SITE_BASE_URL", "")
+    if not base_url and settings.ALLOWED_HOSTS:
+        base_url = f"https://{settings.ALLOWED_HOSTS[0]}"
+    if not base_url:
+        base_url = "http://localhost:8000"
+    base_url = base_url.rstrip("/")
+
+    uid = urlsafe_base64_encode(force_bytes(instance.pk))
+    token = default_token_generator.make_token(instance)
+    activation_link = reverse("activate", kwargs={"uidb64": uid, "token": token})
+    activation_url = f"{base_url}{activation_link}"
+
+    message = render_to_string(
+        "email/activation_email.html",
+        {
             "user": instance,
             "activation_url": activation_url,
-        })
-        send_mail(subject, message, "admin@yourdomain.com", [instance.email])
-
+        },
+    )
+    send_mail(
+        subject="Activate Your Account",
+        message=message,
+        from_email=getattr(settings, "DEFAULT_FROM_EMAIL", None),
+        recipient_list=[instance.email],
+        fail_silently=True,
+    )
